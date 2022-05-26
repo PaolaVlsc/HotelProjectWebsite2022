4408ce8c1ba7f6f790a56aed5cdcd45f96aafe80
urmrs98 <paolavlsc170698@gmail.com>
2022-05-26 19:56:48 +0300

    Συμπλήρωση Αρχικής Σελίδας

--- js/script.js
+++ js/script.js
@@ -9,8 +9,19 @@ menu.onclick = () =>
     navbar.classList.toggle('active');
 }
 
-
+// homepage sections
 window.onscroll = () =>{
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
 };
+
+// header pictures
+var swiper = new Swiper(".home-slider", {
+    loop:true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+
