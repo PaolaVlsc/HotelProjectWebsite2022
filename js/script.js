a1fcd5eb9f77a70642b02b35053dde00fb574c76
urmrs98 <paolavlsc170698@gmail.com>
2022-05-27 00:20:57 +0300

    Add reviews

--- js/script.js
+++ js/script.js
@@ -25,3 +25,20 @@ var swiper = new Swiper(".home-slider", {
 });
 
 
+var swiper = new Swiper(".reviews-slider", {
+    grabCursor:true,
+    loop:true,
+    autoHeight:true,
+    spaceBetween: 20,
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        700: {
+            slidesPerView: 2,
+        },
+        1000: {
+            slidesPerView: 3,
+        },
+    },
+});
