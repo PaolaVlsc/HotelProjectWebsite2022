4c93e802b2bb7a1f91bd762f6f0ac01f4975cc6c
urmrs98 <paolavlsc170698@gmail.com>
2022-06-13 10:06:54 +0300

    footer on php

---
+++ footer.php
@@ -0,0 +1,32 @@
+
+<section class="footer">
+
+    <div class="box-container">
+
+        <div class="box">
+            <h3>Συντομεύσεις</h3>
+            <a href="index.php"> <i class="fas fa-angle-right"></i> Αρχική</a>
+            <a href="rooms.php"> <i class="fas fa-angle-right"></i> Δωμάτια</a>
+            <a href="booking.html"> <i class="fas fa-angle-right"></i> Κράτηση</a>
+            <a href="contact.php"> <i class="fas fa-angle-right"></i> Επικοινωνία</a>
+        </div>
+
+        <div class="box fontmine">
+            <h3>Επικοινωνία</h3>
+            <a href="#"> <i class="fas fa-phone"></i> +211-000-0000 </a> <!--fa-phone sxedio-->
+            <a href="#"> <i class="fas fa-envelope"></i> cs161020@uniwa.gr </a>
+            <a href="#"> <i class="fas fa-envelope"></i> cs16XXXX@uniwa.gr </a>
+        </div>
+
+        <div class="box">
+            <h3>Ακολούθησέ Μας!</h3>
+            <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+            <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+        </div>
+
+    </div>
+
+    <div class="credit"><span style="text-transform: none;"> created by UniWA |  ΠΑΝΑΓΗΣ ΝΙΚΟΛΑΟΣ cs161142  | ΒΕΛΑΣΚΟ ΠΑΟΛΑ cs161020 </span>
+    </div>
+
+</section>
